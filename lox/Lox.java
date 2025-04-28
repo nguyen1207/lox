cf871e338af642612f9fbc3537d64f700713475b
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2025-04-29 00:33:19 +0700

    implement interpreter

--- lox/Lox.java
+++ lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+	private static final Interpreter interpreter = new Interpreter();
 	static boolean hadError = false;
+	static boolean hadRuntimeError = false;
 
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
@@ -27,6 +29,8 @@ public class Lox {
 		run(new String(bytes, Charset.defaultCharset()));
 		if (hadError)
 			System.exit(65);
+		if (hadRuntimeError)
+			System.exit(70);
 	}
 
 	private static void runPrompt() throws IOException {
@@ -50,7 +54,7 @@ public class Lox {
 		Expr expression = parser.parse();
 		if (hadError)
 			return;
-		System.out.println(new AstPrinter().print(expression));
+		interpreter.interpret(expression);
 	}
 
 	static void error(int line, String message) {
@@ -63,6 +67,11 @@ public class Lox {
 		} else {
 			report(token.line, " at '" + token.lexeme + "'", message);
 		}
+	}
+
+	static void runtimeError(RuntimeError error) {
+		System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+		hadRuntimeError = true;
 	}
 
 	private static void report(int line, String where, String message) {
