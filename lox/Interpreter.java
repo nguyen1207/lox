59ee7dd6d69057a673f5e2b5f262a94d0d6a185e
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2025-05-05 21:41:28 +0700

    add function call expression

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -1,9 +1,11 @@
 package lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lox.Expr.Assign;
 import lox.Expr.Binary;
+import lox.Expr.Call;
 import lox.Expr.Grouping;
 import lox.Expr.Literal;
 import lox.Expr.Logical;
@@ -15,7 +17,22 @@ import lox.Stmt.Var;
 import lox.Stmt.While;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-	private Environment environment = new Environment();
+	final Environment globals = new Environment();
+	private Environment environment = globals;
+
+	Interpreter() {
+		globals.define("clock", new LoxCallable() {
+			@Override
+			public int arity() {
+				return 0;
+			}
+
+			@Override
+			public Object call(Interpreter interpreter, List<Object> arguments) {
+				return (double) System.currentTimeMillis() / 1000.0;
+			}
+		});
+	}
 
 	void interpret(List<Stmt> statements) {
 		try {
@@ -227,5 +244,28 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 			execute(stmt.body);
 		}
 		return null;
+	}
+
+	@Override
+	public Object visitCallExpr(Call expr) {
+		Object callee = evaluate(expr.callee);
+
+		List<Object> arguments = new ArrayList<>();
+		for (Expr argument : expr.arguments) {
+			arguments.add(evaluate(argument));
+		}
+
+		if (!(callee instanceof LoxCallable)) {
+			throw new RuntimeError(expr.paren, "Can only call functions and classes.");
+		}
+
+		LoxCallable function = (LoxCallable) callee;
+
+		if (arguments.size() != function.arity()) {
+			throw new RuntimeError(expr.paren,
+					"Expected " + function.arity() + " arguments but got " + arguments.size() + ".");
+		}
+
+		return function.call(this, arguments);
 	}
 }
--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -14,8 +14,9 @@ public class GenerateAst {
 		String outputDir = args[0];
 		defineAst(outputDir, "Expr", Arrays.asList(
 				"Assign : Token name, Expr value",
-						"Binary : Expr left, Token operator, Expr right",
-				"Grouping : Expr expression",
+				"Binary : Expr left, Token operator, Expr right",
+						"Call : Expr callee, Token paren, List<Expr> arguments",
+						"Grouping : Expr expression",
 				"Literal : Object value",
 				"Logical: Expr left, Token operator, Expr right",
 				"Unary : Token operator, Expr right",
