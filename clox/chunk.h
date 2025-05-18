86376fed4de111a0fa97cbfbfeffcee0d00fdb2d
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2025-05-19 00:24:24 +0700

    implement basic bytecode chunk structure and debug utilities

---
+++ clox/chunk.h
@@ -0,0 +1,21 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include <stdint.h>
+
+typedef enum {
+	OP_RETURN, // return from the current function operation
+} OpCode;
+
+typedef struct {
+	int count;
+	int capacity;
+	uint8_t* code;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+
+#endif
