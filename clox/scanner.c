196d01d0aafb66413b98bd90a663acaea107c2e0
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2025-06-06 22:12:25 +0700

    fix scan keyword tokens

--- clox/scanner.c
+++ clox/scanner.c
@@ -119,11 +119,11 @@ static TokenType identifierType() {
 				}
 			}
 			break;
-		case 'i': return checkKeyword(1, 4, "f", TOKEN_IF);
-		case 'n': return checkKeyword(1, 4, "il", TOKEN_NIL);
-		case 'o': return checkKeyword(1, 4, "r", TOKEN_OR);
+		case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+		case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+		case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
 		case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
-		case 'r': return checkKeyword(1, 4, "eturn", TOKEN_RETURN);
+		case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
 		case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
 		case 't':
 			if (scanner.current - scanner.start > 1) {
