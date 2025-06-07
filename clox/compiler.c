436e8256bf91df34eb2c469c87332dbf686bd2bd
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2025-06-07 21:20:21 +0700

    add string

--- clox/compiler.c
+++ clox/compiler.c
@@ -189,6 +189,10 @@ static void number() {
 	emitConstant(NUMBER_VAL(value));
 }
 
+static void string() {
+	emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
+}
+
 static void expression() {
 	parsePrecedence(PREC_ASSIGNMENT);
 }
@@ -250,7 +254,7 @@ ParseRule rules[] = {
     [TOKEN_LESS] = {NULL, binary, PREC_COMPARISION},
     [TOKEN_LESS_EQUAL] = {NULL, binary, PREC_COMPARISION},
     [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
-    [TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+    [TOKEN_STRING] = {string, NULL, PREC_NONE},
     [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
     [TOKEN_AND] = {NULL, NULL, PREC_NONE},
     [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
--- clox/memory.h
+++ clox/memory.h
@@ -1,10 +1,16 @@
 #ifndef clox_memory_h 
 #define clox_memory_h
 
+#include "object.h"
 #include "common.h"
+
+#define ALLOCATE(type, count) \
+	(type*)reallocate(NULL, 0, sizeof(type) * (count))
 
 #define GROW_CAPACITY(capacity) \
 	((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount) \
 	(type*)reallocate(pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount))
@@ -13,5 +19,6 @@
 	reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif
